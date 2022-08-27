9aa747bdf9ca4af4f09fa531a712f8178e8237c7
NikDoe <nikd0e@yandex.ru>
2022-08-27 21:15:56 +0300

    feature(pizza): through data w/ props

--- src/App.js
+++ src/App.js
@@ -16,10 +16,7 @@ function App() {
 					</div>
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
+						<PizzaBlock title="Чизбургер-пицца" price={350} />
 					</div>
 				</div>
 			</div>
