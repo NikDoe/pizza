f347dd91039614cf2a6478d575509137d2bbae15
NikDoe <nikd0e@yandex.ru>
2022-09-02 09:22:50 +0300

    feature(pizza): add logic to sort component
    - map list of sort name;
    - open/close popup;
    - select & view name of sort;

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+
 export default function Sort() {
+	const [activeSort, setActiveSort] = useState(0);
+	const [open, setOpen] = useState(false);
+
+	const sortList = ['популярности', 'цене', 'алфавиту'];
+	const sortName = sortList[activeSort];
+
+	const selectTypeSort = index => {
+		setActiveSort(index);
+		setOpen(false);
+	};
+
 	return (
 		<div className="sort">
 			<div className="sort__label">
@@ -15,15 +28,23 @@ export default function Sort() {
 					/>
 				</svg>
 				<b>Сортировка по:</b>
-				<span>популярности</span>
+				<span onClick={() => setOpen(!open)}>{sortName}</span>
 			</div>
-			<div className="sort__popup">
-				<ul>
-					<li className="active">популярности</li>
-					<li>цене</li>
-					<li>алфавиту</li>
-				</ul>
-			</div>
+			{open && (
+				<div className="sort__popup">
+					<ul>
+						{sortList.map((sortName, index) => (
+							<li
+								key={index}
+								onClick={() => selectTypeSort(index)}
+								className={activeSort === index ? 'active' : ''}
+							>
+								{sortName}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
