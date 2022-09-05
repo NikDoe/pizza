8fc0429bf95b3281a2f2893ec989620d17cc0a46
NikDoe <nikd0e@yandex.ru>
2022-09-05 15:17:27 +0300

    refactor(pizza): refactor styles for media queries
    - add cart component;

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,15 @@
+import CartTop from '../components/CartBlock/CartTop';
+import CartItem from '../components/CartBlock/CartItem';
+import CartBottom from '../components/CartBlock/CartBottom';
+
 export default function Cart() {
 	return (
-		<>
-			<h1>Корзина</h1>
-		</>
+		<div className="cart">
+			<CartTop />
+			<div className="content__items">
+				<CartItem />
+			</div>
+			<CartBottom />
+		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
 				setPizzas(arr);
 				setIsLoading(false);
 			});
+		window.scrollTo(0, 0);
 	}, []);
 
 	return (
